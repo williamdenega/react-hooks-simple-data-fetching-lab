4970cc2158bd8069d9a076d7174b472cc4057050
WilliamDenega <williamdenega@gmail.com>
2022-05-02 17:11:48 -0400

    commit tree 242da6566fa7c666760d053821d32ceb0c02dfa5

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React,{useEffect, useState} from 'react'
+
+
+
+export default function App() {
+
+    const [image, setImage] = useState("")
+    //const [isLoaded, setIsLoaded] = useState(false)
+    useEffect((()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((resp) => resp.json())
+        .then((url) => {
+            console.log(url.message)
+            setImage(url.message)
+          //  setIsLoaded(true)
+        })
+    }), [])
+    if(image === null ) return <p>Loading..</p>
+
+    return (
+    <div>
+        <img 
+            src={image}
+            alt='A Random Dog'
+        />
+
+    </div>
+  )
+}
